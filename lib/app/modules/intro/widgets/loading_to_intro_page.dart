547076cd33c96f23a96f7b2cb23ec901e2fcb960
tinhpham2004 <boydzpro3032004@gmail.com>
2023-02-01 12:41:18 +0700

    test

--- lib/app/modules/intro/widgets/loading_to_intro_page.dart
+++ lib/app/modules/intro/widgets/loading_to_intro_page.dart
@@ -2,6 +2,6 @@ import 'package:get/get.dart';
 import 'package:school_violence_app/app/routes/app_routes.dart';
 
 void loadingToIntroPage() async {
-  await 0.seconds.delay();
-  Get.toNamed(AppRoutes.sign_up);
+  await 2.seconds.delay();
+  Get.toNamed(AppRoutes.intro);
 }
