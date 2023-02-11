736975bc7cda20093d4f17fa1da29ec372670ab8
tranthanhnhan <cutin17vn@gmail.com>
2023-02-11 20:44:07 +0700

    99%UI

--- lib/app/modules/diary/screens/chatscreen.dart
+++ lib/app/modules/diary/screens/chatscreen.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:school_violence_app/app/core/values/app_colors.dart';
+import 'package:school_violence_app/app/modules/diary/widgets/Message_User.dart';
+import 'package:school_violence_app/app/modules/diary/widgets/Message_Expert.dart';
 import 'package:school_violence_app/app/routes/app_routes.dart';
+import 'package:flutter/scheduler.dart';
+import '../diary_controller.dart';
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
-
+  ChatScreen({super.key});
+  
+  
+  final DiaryController ctrl = Get.find<DiaryController>();
+  
   @override
   Widget build(BuildContext context) {
     GlobalKey<FormState> _chatkey = GlobalKey<FormState>();
     TextEditingController _mess = TextEditingController();
+    final ScrollController _controller = ScrollController();
+
+    void _scrollDown() {
+      _controller.animateTo(
+            _controller.position.maxScrollExtent,
+            curve: Curves.easeOut,
+            duration: const Duration(milliseconds: 300),
+          );
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Align(
+        title: const Align(
           alignment: Alignment.topLeft,
           child: Text(
             "Expert",
@@ -51,11 +68,20 @@ class ChatScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(
-            height: 20,
+          Expanded(
+            child: Container(
+                child: Obx(
+                  ()  {
+                    return ListView.builder(
+                      controller: _controller,
+                      itemCount: ctrl.messages.length,
+                      itemBuilder: (context, index) => ctrl.messages[index]);}
+                )),
+              
           ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 52),
+          SizedBox(height: 20,),
+          Container(
+            margin: EdgeInsets.only(bottom: 52),
             child: Form(
               key: _chatkey,
               child: Row(
@@ -69,13 +95,13 @@ class ChatScreen extends StatelessWidget {
                           fillColor: Color(0xFFEFEFEF),
                           filled: true,
                           hintText: "Type a message to Expert ...",
-                          hintStyle: TextStyle(color: Color(0xFFBDBDBD)),
+                          hintStyle: const TextStyle(color: Color(0xFFBDBDBD)),
                           enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
+                              borderSide: const BorderSide(
                                   width: 0, color: Colors.transparent),
                               borderRadius: BorderRadius.circular(8)),
                           focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
+                              borderSide: const BorderSide(
                                   width: 0, color: Colors.transparent),
                               borderRadius: BorderRadius.circular(8)),
                         ),
@@ -96,21 +122,24 @@ class ChatScreen extends StatelessWidget {
                           color: AppColors.primaryColor,
                           borderRadius: BorderRadius.circular(28)),
                       child: ElevatedButton(
-                        onPressed: () {
-                          if (_chatkey.currentState!.validate()) {
+                          onPressed: () {
+                            if (_chatkey.currentState!.validate()) {
+                              ctrl.updateMessages(Message_User(mess: _mess.text));
                               _mess.clear();
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.primaryColor,
-                          shadowColor: AppColors.primaryColorShadow,
-                          elevation: 5,
-                          minimumSize: const Size(300, 40),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0)),
-                        ),
-                        child: Icon(Icons.send_rounded)
-                      ))
+                              SchedulerBinding.instance?.addPostFrameCallback((_) {
+                              _scrollDown();
+                              });
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.primaryColor,
+                            shadowColor: AppColors.primaryColorShadow,
+                            elevation: 5,
+                            minimumSize: const Size(300, 40),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                          ),
+                          child: Icon(Icons.send_rounded)))
                 ],
               ),
             ),
