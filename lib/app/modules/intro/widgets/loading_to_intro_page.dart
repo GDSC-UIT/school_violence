93553b09e7405fbb20eb23a9844d7efcfb80cbfe
Anh Nguyen <anhnguyen150301@gmail.com>
2023-02-13 15:26:51 +0700

    edit loading page

--- lib/app/modules/intro/widgets/loading_to_intro_page.dart
+++ lib/app/modules/intro/widgets/loading_to_intro_page.dart
@@ -3,5 +3,5 @@ import 'package:school_violence_app/app/routes/app_routes.dart';
 
 void loadingToIntroPage() async {
   await 0.seconds.delay();
-  Get.toNamed(AppRoutes.findFriends);
+  Get.toNamed(AppRoutes.home);
 }
