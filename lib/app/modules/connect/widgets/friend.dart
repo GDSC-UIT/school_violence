87f09b201883d058d20b150733bec8ebcab5063c
Anh Nguyen <anhnguyen150301@gmail.com>
2023-02-13 14:48:18 +0700

    Connect page

---
+++ lib/app/modules/connect/widgets/friend.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class Friend {
+  String name;
+  String phoneNumber;
+  String avatarLink;
+
+  Friend(
+      {required this.name,
+      required this.phoneNumber,
+      required this.avatarLink}) {}
+}
---
+++ lib/app/modules/connect/widgets/list_friend.dart
@@ -0,0 +1,14 @@
+import 'package:school_violence_app/app/modules/connect/widgets/friend.dart';
+
+class ListFriend {
+  List<Friend> listFriend = [
+    Friend(
+        name: 'Friend',
+        avatarLink: 'assets/images/grey-avatar.png',
+        phoneNumber: '+84 123456789'),
+    Friend(
+        name: 'Friend 2',
+        avatarLink: 'assets/images/lock.png',
+        phoneNumber: '+84 1212356789'),
+  ];
+}
