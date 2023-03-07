e33dc005ad9c18706a9a61bc8506c9b36ef5a569
tranthanhnhan <cutin17vn@gmail.com>
2023-03-07 22:11:01 +0700

    merge fixed chat

--- lib/app/modules/diary/diary_controller.dart
+++ lib/app/modules/diary/diary_controller.dart
@@ -7,7 +7,8 @@ import 'package:school_violence_app/app/modules/diary/widgets/Message_Orther.dar
 class DiaryController extends GetxController {
   RxList messages = [ ].obs;
   RxString userId = ''.obs;
-
+  RxBool haveText = false.obs;
+  RxBool isFetchingExpert = false.obs;
   void updateMessages(var mess){
     messages.value=mess;
   }
